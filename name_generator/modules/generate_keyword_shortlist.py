bf379f6aae2a25ec122ba94507c8a9c24fc76c2b
Korbinian Habereder <haberederkorbinian@googlemail.com>
2023-07-09 12:50:01 +0200

    chore: removed unnecessary print

--- name_generator/modules/generate_keyword_shortlist.py
+++ name_generator/modules/generate_keyword_shortlist.py
@@ -50,8 +50,7 @@ def generate_keyword_shortlist(keyword_data):
                 keyword_class="prime",
                 shortlist=keyword["shortlist"]
             )
-            print(keyword)
-
+            
             shortlist.append(keyword_obj)
 
     return shortlist
