626abe2cf423f18ea7c7a22920ed9be48b720d06
Korbinian Habereder <haberederkorbinian@googlemail.com>
2023-07-18 17:47:44 +0200

    feat: added deta domain logger

--- deta_integrations/upload_simplified_eng_dict.py
+++ deta_integrations/upload_simplified_eng_dict.py
@@ -1,10 +1,9 @@
 from deta import Deta
-import orjson
 
 d = Deta("a0xztrxeaye_oCyrV2ZZAKZqn4fw7NEG8hhJdn56YMau")
 
 drive = d.Drive("eng_simplified")
 
-with open("../wordsAPI/simplified_eng_dict.json", "r") as upload_fp:
+with open("../../wordsAPI/simplified_eng_dict.json", "r") as upload_fp:
 
     drive.put("simplified_eng_dict.json", upload_fp)
